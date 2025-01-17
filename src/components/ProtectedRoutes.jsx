b6e6a311c13b66377be14f873a9e651323297b74
umais-rehman <umaisrehman109@gmail.com>
2025-01-18 00:35:27 +0500

    complete

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-// Protected Route Component
 const ProtectedRoute = () => {
-  const token = localStorage.getItem("token"); // Get token from localStorage
+  const token = localStorage.getItem("token"); 
 
   if (!token) {
-    // If token is not found, redirect to login page
     return <Navigate to="/login" />;
   }
 
-  // If token exists, render the protected page
   return <Outlet />;
 };
 
